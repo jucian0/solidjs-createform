3e3505233684e139b7d2fac379618d3d3f02140f
Jucian0 <juciano@outlook.com.br>
2022-07-10 09:43:19 -0300

    feat(createinitialerrors): done createInitialErros function
    This function should create a deep clone of initialValues, replacing the prinitives values.

--- src/CreateInitialErrors.ts
+++ src/CreateInitialErrors.ts
@@ -1,10 +1,5 @@
-export function createInitialErrors<T extends {}>(values: T) {
-   for (let key in values) {
-      if (typeof values[key] === 'object') {
-         values[key] = createInitialErrors(values[key])
-      }
-      values[key] = ''
-   }
+import { replacePrimitivesInObject } from './ObjectUtils'
 
-   return values
+export function createInitialErrors<T extends {}>(values: T) {
+   return replacePrimitivesInObject(values, '')
 }
