dd0662b5200710d822a2576245ef9bd0fa5ab540
Jucian0 <juciano@outlook.com.br>
2022-07-10 09:44:50 -0300

    feat(createinitialtouched): done createInitialTouched function
    This function should create a deep clone of initialValues, replacing primitives values for a boolean
    value; false

--- src/CreateInitialTouched.ts
+++ src/CreateInitialTouched.ts
@@ -1,13 +1,5 @@
+import { replacePrimitivesInObject } from './ObjectUtils'
+
 export function createInitialTouched<T extends {}>(values: T) {
-   function evaluate(partial: any) {
-      for (const key in partial) {
-         if (typeof partial[key] === 'object') {
-            partial[key] = evaluate(partial[key])
-            console.log(partial[key])
-         }
-         partial[key] = false
-      }
-      return partial
-   }
-   return evaluate(values)
+   return replacePrimitivesInObject(values, false)
 }
